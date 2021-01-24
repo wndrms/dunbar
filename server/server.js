0d8daa1b657d1d0f764c4e5e988d2336e6b7cfc1
정중근 <jeongjung-geun@jeongjung-geun-ui-MacBookPro.local>
2021-01-24 18:21:45 +0900

    21.01.24.18:20

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const pool = require("./pool");
 const app = express();
 
+var corsOptions = {	
+    origin: "http://localhost:3000"	
+};	
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
